77bdd4c86b49951110ebbd9752f667a8b5028616
Alberto Peripolli <info@albertoperipolli.com>
2024-01-05 14:37:48 +0100

    add phpdoc

--- src/Facades/HubSpot.php
+++ src/Facades/HubSpot.php
@@ -4,6 +4,18 @@ namespace Tecnobit\LaravelHubSpot\Facades;
 
 use Illuminate\Support\Facades\Facade;
 
+/**
+ * @method static \HubSpot\Discovery\Auth\Discovery                     auth()
+ * @method static \HubSpot\Discovery\Automation\Discovery               automation()
+ * @method static \HubSpot\Discovery\Cms\Discovery                      cms()
+ * @method static \HubSpot\Discovery\Conversations\Discovery            conversations()
+ * @method static \HubSpot\Discovery\CommunicationPreferences\Discovery communicationPreferences()
+ * @method static \HubSpot\Discovery\Crm\Discovery                      crm()
+ * @method static \HubSpot\Discovery\Events\Discovery                   events()
+ * @method static \HubSpot\Discovery\Files\Discovery                    files()
+ * @method static \HubSpot\Discovery\Marketing\Discovery                marketing()
+ * @method static \HubSpot\Discovery\Webhooks\Discovery                 webhooks()
+ */
 class HubSpot extends Facade
 {
 	/**
@@ -13,6 +25,6 @@ class HubSpot extends Facade
 	 */
 	protected static function getFacadeAccessor()
 	{
-		return 'Tecnobit\LaravelHubSpot\HubSpot';
+		return \Tecnobit\LaravelHubSpot\HubSpot::class;
 	}
 }
